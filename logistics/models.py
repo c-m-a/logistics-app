c1f37222859b3922b18df7e2deca80308a239030
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2024-02-18 12:19:33 -0500

    Add: Customer models, views, urls and settings

--- logistics/models.py
+++ logistics/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Customer(models.Model):
+    id = models.CharField(max_length=128, default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    fullname = models.CharField(max_length=128)
+    address = models.CharField(max_length=128)
+    phone = models.CharField(max_length=32)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.fullname)
+
+    class Meta:
+        ordering = ['fullname']
+
